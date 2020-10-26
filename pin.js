2f30f70fd4564cb9b9d53c64b7e80f255e636974
Sebastian-muchui <sebastianmuchui79@gmail.com>
2020-10-26 14:56:27 +0100

    a simple password checking system in javascript

---
+++ pin.js
@@ -0,0 +1,13 @@
+// // Function to check pin
+var presetpin = 29366464;
+var pin = 0;
+var i = 0;
+while (pin < presetpin ) {
+    window.prompt('Enter pin');
+    if(pin = presetpin){
+        window.alert('Welcome to my website');
+    }else{
+        alert('Wrong pin');
+    }
+    i=10;
+}
